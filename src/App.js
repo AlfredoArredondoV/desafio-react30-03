d6e9b96062961dabd2c9b4bb5e1f0f42dda541fe
alfredo <alfredo.arredondo@hotmail.com>
2023-04-16 22:56:01 -0400

    Desafio_React30_03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+// import Header from "./components/Header";
+import Form from "./components/Form";
+import Footer from "./components/Footer";
+import Item from "./components/Item.jsx";
+import {BaseColaboradores} from "./components/Colaboradores"
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState(BaseColaboradores);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const onSubmit = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const handleSearch = (search) => {
+    search.preventDefault();
+    const newArray = todos.filter((item) => {return Object.values(item).join('').toLowerCase().includes(search.toLowerCase())} );
+    console.log("me diste click");
+    console.log(search);
+    setTodos(newArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='navbar'>
+            <p className='fs-1 text-white p-2'>Buscador de Colaboradores</p>
+            <form className="d-flex p-2" role="search" >
+                <input 
+                    className="form-control me-2" 
+                    type="search" 
+                    placeholder="Search" 
+                    name="search"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                    />
+                <button className="btn btn-outline-success" type="submit"  onClick={() =>handleSearch({search})}>Search</button>
+            </form>
+        </div>
+
+      {/* <Header titulo="Buscador de Colaboradores"/> */}
+      <Form onSubmit={onSubmit} />
+      <Footer titulo="Lista de Colaboradores"/>
+      <ul className="list-group">
+        {todos.map((todo) => {
+          return (
+            <Item
+              key={todo.id}
+              todo={todo}
+              handleSearch={handleSearch}
+            />
+          );
+        })}
+      </ul>
+      
     </div>
   );
 }
